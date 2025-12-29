9a28586d6730987eecdf10d78dd2a0b98b2aeedf
Him1nshu <astng888@gmail.com>
2025-12-29 19:37:47 +0530

    added cTOk

--- temp convert/index.js
+++ temp convert/index.js
@@ -1,15 +1,31 @@
-
 function convert(){
-    let inpt=document.getElementById("inputt").value;
-    let out=document.getElementById("result");
-    let ff=document.getElementById("from");
-    let err1=document.getElementById("dboxer");
+    let inpt = document.getElementById("inputt").value;
+    let out = document.getElementById("result");
+    let ff = document.getElementById("from");
+    let tt = document.getElementById("to");
+    let err1 = document.getElementById("dboxer");
+    let err2 = document.getElementById("dboxer2");
 
-    
-    if(inpt.length===0)out.innerText="no input";
+    err1.innerText = "";
+    err2.innerText = "";
+    out.innerText = "";
+
+    if (inpt.length === 0) {
+        out.innerText = "no input";
+        
+    }
+
     if (ff.value === "") {
-        err1.innerText = "❌ No option selected";
-    } else {
-        err1.innerText = "✅ Selected: " + select.value;
+        err1.innerText = "No FROM option selected";
+        
+    }
+
+    if (tt.value === "") {
+        err2.innerText = "No TO option selected";
+    }
+
+    // Celsius → Kelvin
+    if (ff.value === "cl" && tt.value === "kl") {
+        out.innerText = inpt+"C="+Number(inpt) + 273.15+"K";
     }
 }
