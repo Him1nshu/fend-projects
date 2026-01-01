3568d7082e490cb7511c1efda905ddb91941d416
Him1nshu <astng888@gmail.com>
2026-01-01 22:34:57 +0530

    finalized temp converter

--- temp convert/index.js
+++ temp convert/index.js
@@ -24,8 +24,52 @@ function convert(){
         err2.innerText = "No TO option selected";
     }
 
-    // Celsius -> Kelvin
-    if (ff.value === "cl" && tt.value === "kl") {
+    
+    if (ff.value === tt.value && tt.value!="" && ff.value!="") {
+        out.innerText ="same type conversion";
+        }
+
+        
+    // if (ff.value === "fn" && tt.value === "fn") {
+    //     out.innerText ="same type conversion";
+    //     }
+
+        
+    // if (ff.value === "kl" && tt.value === "kl") {
+    //     out.innerText ="same type conversion";
+    //     }
+
+    
+    // if (ff.value === "cl" && tt.value === "cl") {
+    //     out.innerText ="same type conversion";
+    //     }
+
+    else if (ff.value === "cl" && tt.value === "kl") {
         out.innerText = inpt+"C="+(Number(inpt) + 273.15)+"K";
     }
+    
+    else if (ff.value === "cl" && tt.value === "fn") {
+        out.innerText = inpt+"C="+((Number(inpt)*(9/5)) + 32)+"F";
+    }
+
+    else if (ff.value === "kl" && tt.value === "cl") {
+        out.innerText = inpt+"K="+(Number(inpt) - 273.15)+"C";
+    }
+    
+    else if (ff.value === "kl" && tt.value === "fn") {
+        out.innerText = inpt+"K="+((Number(inpt) - 273.15)*(9/5)+32)+"F";
+    }
+
+
+    else if (ff.value === "fn" && tt.value === "cl") {
+        out.innerText =inpt+"F="+((Number(inpt) - 32)*(5/9))+"C";
+        }
+
+    else if (ff.value === "fn" && tt.value === "kl") {
+        out.innerText =inpt+"F="+((Number(inpt) - 32)*(5/9)+273.15)+"K";
+        }
+
+
+    
+    
 }
