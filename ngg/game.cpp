cf8962c0ad4f75a9748d8fef68c89b3de6abbe08
Him1nshu <astng888@gmail.com>
2026-02-24 22:35:02 +0530

    added number guessing game
    initial commit

---
+++ ngg/game.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+cout<<"Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number."<<endl;
+cout<<"Please select the difficulty level:\n1. Easy (10 chances)\n2. Medium (5 chances)\n3. Hard (3 chances)"<<endl;
+int ch=0;
+cout<<"enter you choice="<<endl;
+cin>>ch;
+// switch(ch){
+
+// }
+return 0;
+}
